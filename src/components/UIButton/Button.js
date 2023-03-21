e1d380d66e90a355be60c181fa107eb9e0a7ee4a
Karina <karina.steinberg@voco.ee>
2023-03-21 17:12:45 +0200

    Use dynamic CSS modules for CourseInput

--- src/components/UIButton/Button.js
+++ src/components/UIButton/Button.js
@@ -1,41 +1,15 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
-import Button from '../../UIButton/Button';
+import styles from './CourseInput.module.css'
+//import styled from "styled-components";
 
-const  FormControl = styled.div`
-  margin: 0.5rem 0;
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? 'red' : 'black'};
-  }
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-    background: ${props => props.invalid ? 'salmon' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-  &.invalid input {
-    border-color: red;
-    background: salmon;
-  }
-  &.invalid label {
-    color: red;
-  }`
+
+import Button from './Button';
 
 const CourseInput = props => {
     const [enteredValue, setEnteredValue] = useState('');
-
     const [isValid, setIsValid] = useState(true)
+
     const goalInputChangeHandler = event => {
         if (event.target.value.trim().length > 0) {
             setIsValid(true)
@@ -54,10 +28,10 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl invalid={!isValid}>
+            <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler} />
-            </FormControl>
+            </div>
             <Button type="submit">Add Goal</Button>
         </form>
     );
